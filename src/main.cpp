b3a4b3d49486f4d536d5e6682fcd062589cead2c
François Coppens <mail@fmgjcoppens.eu>
2024-08-11 15:56:09 +0200

    fmt and spdlog are compiling via CPM. Can be successfully included and called.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <fmt/core.h>
+#include "spdlog/spdlog.h"
 #include <GLFW/glfw3.h>
 
 int main (int argc, char *argv[]) {
     fmt::print("We're up and running!\n");
+    spdlog::info("Welcome to spdlog!");
     return 0;
 }
